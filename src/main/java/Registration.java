3f9f7960656bfe2213ed41d1ea0dbce48022be5f
Katya9999 <ekaterina.biryukova97@mail.ru>
2021-05-07 15:23:22 +0300

    добавлена регистрация

---
+++ src/main/java/Registration.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Registration {
+    private static final File dirCreds = new File("src/main/resources/Credentials");
+    private static File currentFile;
+
+    public static File getFile() {
+        return currentFile;
+    }
+
+    public static boolean isRegistration(String login){
+        for (File file: Objects.requireNonNull(dirCreds.listFiles())){
+            try(Scanner scanner = new Scanner(file)) {
+                String s;
+                while (scanner.hasNext()){
+                    s = scanner.nextLine();
+                    if(s.contains("login")){
+                        s = s.split(": ")[1];
+                        if(login.equals(s)){
+                            currentFile = file;
+                            return true;
+                        }
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static void register(String login, String password){
+        try {
+            Files.createFile(Path.of("src/main/resources/Credentials/" + login + ".txt"));
+            Files.writeString(Path.of("src/main/resources/Credentials/" + login + ".txt"), "login: " + login + "\n" + "password: " + password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
